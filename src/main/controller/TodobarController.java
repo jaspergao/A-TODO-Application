82512118eb29f6f0b9ae6ab4a3097deda9d970a7
Jasz <v4k2b@ugrad.cs.ubc.ca>
2019-03-18 13:27:59 -0700

    Completed First Task Completed Read/Write for Task 2 On Task 4, has font issue like other students on discourse Task 4 completed Task 5 completed

--- src/main/controller/TodobarController.java
+++ src/main/controller/TodobarController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXRippler;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -26,6 +27,8 @@ import java.util.ResourceBundle;
 public class TodobarController implements Initializable {
     private static final String todoOptionsPopUpFXML = "resources/fxml/TodoOptionsPopUp.fxml";
     private static final String todoActionsPopUpFXML = "resources/fxml/TodoActionsPopUp.fxml";
+    private File todoOptionsPopUpFxmlFile = new File(todoOptionsPopUpFXML);
+    private File todoActionsPopUpFxmlFile = new File(todoActionsPopUpFXML);
     
     @FXML
     private Label descriptionLabel;
@@ -39,6 +42,8 @@ public class TodobarController implements Initializable {
     private StackPane todoOptionsPopUpBurger;
     
     private Task task;
+    private JFXPopup todoBarActionPopUp;
+    private JFXPopup viewOptionPopUp;
     
     // REQUIRES: task != null
     // MODIFIES: this
@@ -51,6 +56,110 @@ public class TodobarController implements Initializable {
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO: complete this method
+        loadTodoActionsPopUp();
+        loadTodoBarPopUpActionListener();
+        loadTodoOptionsPopUp();
+        loadTodoOptionsPopUpActionListener();
     }
+
+//     EFFECTS: load options pop up (to do, up next, in progress, done, pomodoro!)
+    private void loadTodoActionsPopUp() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(todoActionsPopUpFxmlFile.toURI().toURL());
+            fxmlLoader.setController(new TodoActionsPopUpController());
+            todoBarActionPopUp = new JFXPopup(fxmlLoader.load());
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    // EFFECTS: load actions pop up (edit, delete)
+    private void loadTodoOptionsPopUp() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(todoOptionsPopUpFxmlFile.toURI().toURL());
+            fxmlLoader.setController(new TodoBarOptionsPopUpController());
+            viewOptionPopUp = new JFXPopup(fxmlLoader.load());
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    // EFFECTS: show option selector pop up when its icon is clicked
+    private void loadTodoOptionsPopUpActionListener() {
+        todoOptionsPopUpBurger.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                viewOptionPopUp.show(todoOptionsPopUpBurger,
+                        JFXPopup.PopupVPosition.TOP,
+                        JFXPopup.PopupHPosition.LEFT,
+                        12,
+                        15);
+            }
+        });
+    }
+
+    // EFFECTS: show actions pop up when its icon is clicked
+    private void loadTodoBarPopUpActionListener() {
+        todoActionsPopUpBurger.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                todoBarActionPopUp.show(todoActionsPopUpBurger,
+                        JFXPopup.PopupVPosition.TOP,
+                        JFXPopup.PopupHPosition.RIGHT,
+                        -12,
+                        15);
+            }
+        });
+    }
+
+    // Inner class: view option pop up controller
+    class TodoBarOptionsPopUpController {
+        @FXML
+        private JFXListView<?> optionPopUpList;
+
+        @FXML
+        private void submit() {
+            int selectedIndex = optionPopUpList.getSelectionModel().getSelectedIndex();
+            switch (selectedIndex) {
+                case 0:
+                    Logger.log("TodobarOptionsPopUpController", "Edit is not supported in this version");
+                    break;
+                case 1:
+                    Logger.log("TodobarOptionsPopUpController", "Delete is not supported in this version");
+                    break;
+                default:
+                    Logger.log("TodobarOptionsPopUpController", "No action is implemented for the selected option");
+            }
+            viewOptionPopUp.hide();
+        }
+    }
+
+    // Inner class: action pop up controller
+    class TodoActionsPopUpController {
+        @FXML
+        private JFXListView<?> actionPopUpList;
+
+        @FXML
+        private void submit() {
+            int selectedIndex = actionPopUpList.getSelectionModel().getSelectedIndex();
+            switch (selectedIndex) {
+                case 0:
+                    Logger.log("TodobarActionsPopUpController", "To do is not supported in this version");
+                    break;
+                case 1:
+                    Logger.log("TodobarActionsPopUpController", "Up next is not supported in this version");
+                    break;
+                case 2:
+                    Logger.log("TodobarActionsPopUpController", "In progress is not supported in this version");
+                    break;
+                case 3:
+                    Logger.log("TodobarActionsPopUpController", "Done is not supported in this version");
+                    break;
+                default:
+                    Logger.log("TodobarActionsPopUpController", "No action is implemented for the selected option");
+            }
+            todoBarActionPopUp.hide();
+        }
+    }
+
 }
